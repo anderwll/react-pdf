e7756a4b9ba7cec55586f63d7b03e11aef08946a
anderwll <ander.wilmsen98@gmail.com>
2024-03-07 23:03:35 -0300

    feat: add example pdf

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -12,7 +12,7 @@ export function Modal({ open, onClose }: ModalProps) {
       open={open}
       className="absolute top-0 right-0 w-screen h-screen flex flex-col justify-center items-center"
     >
-      <div className="w-[768px] h-[768px] bg-zinc-800 rounded relative z-50 p-12">
+      <div className="w-[640px] h-[768px] bg-zinc-800 rounded relative z-50 p-12">
         <button
           onClick={onClose}
           className="absolute right-0 top-0 bg-transparent"
